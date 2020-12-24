8ef4721b7d1b686484f2323c1afde46d2476fdfb
rodrigo.resque <rodrigo.resque@tre-pa.jus.br>
2020-12-24 18:53:14 -0300

    updt

--- advinhacao.py
+++ advinhacao.py
@@ -1,22 +1,48 @@
-print("Bem vindo ao jogos de adivinacao!")
+import random
 
-numero_secreto = 42
+print("***********************************")
+print("Bem Vindo ao Jogo de Adivinhacao!")
+print("***********************************")
 
-chute_str = input("Digite o seu numero: ")
+numero_secreto = random.randrange(1, 101, 1)
+total_de_tentativas = 0
+pontos = 1000
 
-print("Voce digitou: ", chute_str)
+print("Qual o nuvel de dificuldade ?", numero_secreto)
+print("(1) Facil (2) Medio (3) Dificil")
 
-chute_int = int(chute_str)
+nivel = int(input("Defina o nivel: "))
 
-acertou = chute_int == numero_secreto
-maior   = chute_int > numero_secreto
-menor   = chute_int < numero_secreto
-
-if(acertou):
-    print("Voce acertou")
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
 else:
-    if(maior):
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    elif(menor):
-        print("Você errou! O seu chute foi menor que o número secreto.")
+    total_de_tentativas = 5
+
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite um numero entre 1 e 100: ")
+    print("Voce digitou ", chute_str)
+    chute = int(chute_str)
+    if chute < 1 or chute > 100:
+        print("Voce deve digitat entre 1 e 100!")
+        continue
+
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+
+    if acertou:
+        print("Voce acertou, o total de pontos e {}".format(pontos))
+        break
+    else:
+        if maior:
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif menor:
+            print("Você errou! O seu chute foi menor que o número secreto.")
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos = pontos - pontos_perdidos
+    rodada = rodada + 1
+
 print("Fim do jogo")
